6ee2fc8fa6822f3db355db3534ed5745a553ecd0
Xu Wenjing <80949489+p1anx@users.noreply.github.com>
2024-08-12 00:28:39 +0800

    Update nerd_fonts.sh

--- nerd_fonts.sh
+++ nerd_fonts.sh
@@ -5,9 +5,9 @@ mkdir -p ~/.local/share/fonts
 
 cd ~/.local/share/fonts && curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
 
-x JetBrainsMono.tar.xz
+tar -xJvf JetBrainsMono.tar.xz
 
 curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Mononoki.tar.xz
 
-x Mononoki.tar.xz
+tar -xJvf Mononoki.tar.xz
 
