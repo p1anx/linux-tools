bf4c87781f8a8e0216dd10c6d37ca27fa454058d
Xu Wenjing <80949489+p1anx@users.noreply.github.com>
2024-08-22 21:45:55 +0800

    Create docker_proxy.sh

---
+++ docker_proxy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ ! -f "/etc/systemd/system/docker.service.d/http-proxy.conf" ]; then
+  sudo mkdir -p /etc/systemd/system/docker.service.d
+  sudo touch /etc/systemd/system/docker.service.d/http-proxy.conf
+  cat > http-proxy.conf <<EOF
+[Service]
+Environment="HTTP_PROXY=http://127.0.0.1:10809/"
+Environment="HTTPS_PROXY=http://127.0.0.1:10809/"
+EOF
+  echo "docker proxy is configured"
+else
+  echo "http-proxy.conf exist"
+fi
+sudo systemctl daemon-reload
+sudo systemctl restart docker
