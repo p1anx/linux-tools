0c027fc58f83f5c2a520dc12c50e7e0ba6a3c7f9
Xu Wenjing <2514034568@qq.com>
2024-08-23 00:28:07 +0800

    update

--- docker/docker_config.sh
+++ docker/docker_config.sh
@@ -64,7 +64,7 @@ esac
 if [ ! -f "/etc/systemd/system/docker.service.d/http-proxy.conf" ]; then
   sudo mkdir -p /etc/systemd/system/docker.service.d
   sudo touch /etc/systemd/system/docker.service.d/http-proxy.conf
-  cat >> http-proxy.conf << EOF
+  sudo tee -a http-proxy.conf << EOF
 [Service]
 Environment="HTTP_PROXY=http://127.0.0.1:10809/"
 Environment="HTTPS_PROXY=http://127.0.0.1:10809/"
