8b6bcce9b8a1238ae69a1a19006092809aa8e5dd
p1anx <2514034568@qq.com>
2024-08-28 12:39:43 +0800

    add wez.sh

---
+++ wez.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
+echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+sudo apt update
+sudo apt install -y wezterm
+
+git clone https://github.com/KevinSilvester/wezterm-config.git ~/.config/wezterm
