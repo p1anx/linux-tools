50483169d804b0f1480c0a369edba9e211c43404
p1anx <2514034568@qq.com>
2024-08-29 15:46:08 +0800

    update tmux.sh

--- tmux/tmux.sh
+++ tmux/tmux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir -p "$HOME"/tmux_repo
-OH_MY_TMUX_DIR="$HOME/tmux_repo"
+mkdir -p "$HOME"/oh_my_tmux
+OH_MY_TMUX_DIR="$HOME/oh_my_tmux"
 git clone https://github.com/p1anx/.tmux.git $OH_MY_TMUX_DIR
 mkdir -p ~/.config/tmux
 ln -s $OH_MY_TMUX_DIR/.tmux.conf ~/.config/tmux/tmux.conf
