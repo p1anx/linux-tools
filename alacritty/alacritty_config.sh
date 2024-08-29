1e3310669a2e8a5a606bc7f39720f4a0313b9b71
p1anx <2514034568@qq.com>
2024-08-29 18:26:32 +0800

    install and config alacritty

---
+++ alacritty/alacritty_config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# We use Alacritty's default Linux config directory as our storage location here.
+
+mkdir -p ~/.config/alacritty/themes
+git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
