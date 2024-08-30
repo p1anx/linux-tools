144be3bc3bae44de35ef4cee95739906d9607d5a
tuf <2514034568@qq.com>
2024-08-30 15:58:21 +0800

    update

--- nerd_fonts.sh
+++ nerd_fonts.sh
@@ -4,10 +4,10 @@
 mkdir -p ~/.local/share/fonts
 
 cd ~/.local/share/fonts && curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz
-
 tar -xJvf JetBrainsMono.tar.xz
 
 curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Mononoki.tar.xz
-
 tar -xJvf Mononoki.tar.xz
 
+curl -OL https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.tar.xz
+tar -xJvf FiraCode.tar.xz
