64654adf618edafb4b40cddf8836acbac7f6c9e7
tuf <2514034568@qq.com>
2024-08-30 21:53:27 +0800

    add fzf.sh

---
+++ fzf.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+te -a ~/.zshrc <<EOF
+fd() {
+  local dir
+  dir=$(find ${1:-.} -path '*/\.*' -prune \
+  -o -type d -print 2>/dev/null | fzf +m) &&
+  cd "$dir"
+}
+export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --border --margin=1 --padding=1"
+EOF
+
+echo "===================="
+echo "fzf is installed"
+echo "===================="
