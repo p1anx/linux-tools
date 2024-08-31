bebf960eb10fc9628c5f5e82a1a069f9213cef50
tuf <2514034568@qq.com>
2024-08-31 16:15:22 +0800

    update ala.sh

--- alacritty/alacritty.sh
+++ alacritty/alacritty.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-DIR="$(pwd)"
+local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 function alacritty_install() {
   sudo apt update
@@ -9,29 +8,29 @@ function alacritty_install() {
   source "$HOME/.cargo/env"
 
   git clone https://github.com/alacritty/alacritty.git $HOME
-  cd $HOME/alacritty
 
   sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 -y
   cargo build --release
 
+  local tmp_dir="$HOME/alacritty"
   #set desktop entry for alacritty
-  sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
-  sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
-  sudo desktop-file-install extra/linux/Alacritty.desktop
+  sudo cp "$tmp_dir"/target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+  sudo cp "$tmp_dir"/extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+  sudo desktop-file-install "$tmp_dir"/extra/linux/Alacritty.desktop
   sudo update-desktop-database
 
-  rm -rf $HOME/alacritty
 }
 
 function alacritty_config() {
   # change the icon of alacritty
-  cd "$DIR"
+  cd "$script_dir"
   sudo cp Alacritty.svg /usr/share/pixmaps/
 
   # set the configuration for alacritty
   mkdir -p ~/.config/alacritty/themes
   git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
   cp alacritty.toml ~/.config/alacritty/
+  rm -rf $HOME/alacritty
 }
 
 function alacritty() {
