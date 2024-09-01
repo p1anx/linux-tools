057c33ea325a81986d8d09920d95584521fdaeb5
rk-test <rk-test@192.168.189.130>
2024-09-01 15:46:24 +0800

    update conda and tmux

--- python/miniconda.sh
+++ python/miniconda.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 function miniconda() {
-  local conda_dir=~/miniconda3
-  if [ ! -e "$conda_dir" ]; then
-
-    mkdir -p "$conda_dir"
-    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O "$conda_dir"/miniconda.sh
-    bash "$conda_dir"/miniconda.sh -b -u -p "$conda_dir"
-    rm -rf "$conda_dir"/miniconda.sh
-
-    "$conda_dir"/bin/conda init bash
-    "$conda_dir"/bin/conda init zsh
-  else
-    echo "conda is already installed"
+  local conda_dir="$HOME/miniconda3"
+  if [ -e "$conda_dir" ]; then
+    rm -rf $conda_dir
   fi
+  mkdir -p "$conda_dir"
+  wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O "$conda_dir"/miniconda.sh
+  bash "$conda_dir"/miniconda.sh -b -u -p "$conda_dir"
+  rm -rf "$conda_dir"/miniconda.sh
+
+  "$conda_dir"/bin/conda init bash
+  "$conda_dir"/bin/conda init zsh
   #cancel "base"
   conda config --set auto_activate_base false
 
--- tmux/tmux.sh
+++ tmux/tmux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-function tmux() {
+function tmux_bak() {
 
   if grep -q "ubuntu" /etc/os-release; then
     sudo apt update
@@ -14,10 +14,46 @@ function tmux() {
   fi
 
   local OH_MY_TMUX_DIR="$HOME/oh_my_tmux"
-  mkdir -p $OH_MY_TMUX_DIR
-  git clone https://github.com/p1anx/.tmux.git $OH_MY_TMUX_DIR
-  mkdir -p ~/.config/tmux
-  ln -s $OH_MY_TMUX_DIR/.tmux.conf ~/.config/tmux/tmux.conf
-  cp $OH_MY_TMUX_DIR/.tmux.conf.local ~/.config/tmux/tmux.conf.local
+  #delete the file for reinstall
+  if [ -d "$OH_MY_TMUX_DIR" ]; then
+    rm -rf $OH_MY_TMUX_DIR
+  fi
+  if [ -d "~/.config/tmux" ]; then
+    rm -rf ~/.config/tmux
+  fi
+  if [ -f "$OH_MY_TMUX_DIR/.tmux.conf" ]; then
+    rm $OH_MY_TMUX_DIR/.tmux.conf
+  fi
+  # mkdir -p $OH_MY_TMUX_DIR
+
+  # git clone https://github.com/gpakosz/.tmux.git $OH_MY_TMUX_DIR
+  # mkdir -p ~/.config/tmux
+  # ln -s $OH_MY_TMUX_DIR/.tmux.conf ~/.config/tmux/tmux.conf
+  # cp $OH_MY_TMUX_DIR/.tmux.conf.local ~/.config/tmux/tmux.conf.local
+
+}
+
+function tmux() {
+  if grep -q "ubuntu" /etc/os-release; then
+    sudo apt update
+    sudo apt install tmux -y
+  elif grep -q "rocky" /etc/os-release; then
+    sudo yum update
+    sudo yum install tmux -y
+    sudo yum install perl -y
+  else
+    echo "The system is not supported"
+    exit
+  fi
+
+  if [ -d "$HOME/.tmux" ]; then
+    rm -rf $HOME/.tmux
+    rm -rf $HOME/.tmux.conf
+    rm -rf $HOME/.tmux.conf.local
+  fi
+  cd
+  git clone https://github.com/p1anx/.tmux.git
+  ln -s -f .tmux/.tmux.conf
+  cp .tmux/.tmux.conf.local .
 
 }
