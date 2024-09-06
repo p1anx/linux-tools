998504cdf2a459b949481184a8e5c0d4c17c0c3c
tuf <2514034568@qq.com>
2024-09-06 21:28:54 +0800

    add nvm and essential.sh

---
+++ others/essential.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+function debian_like() {
+  sudo update
+  sudo apt install ripgrep
+}
+
+function fedora_like() {
+
+}
---
+++ others/nvm.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# install nvm for managment node and npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
