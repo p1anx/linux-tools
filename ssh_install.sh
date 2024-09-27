a2d1db4586fedc0bf4c3ebd6e8a66dc4a0a51f15
xwj <sheer@XWJ-TUF.localdomain>
2024-09-27 09:31:33 +0800

    add ssh for arch

--- ssh_install.sh
+++ ssh_install.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
+function ssh_arch(){
+    sudo pacman -S openssh
+    sudo systemctl start sshd
+    sudo systemctl enable sshd
+}
+function ssh_install(){
+    if grep -q "ubuntu" /etc/os-release; then
+        echo "waiting..."
+    elif grep -q "arch" /etc/os-release; then
+        ssh_arch
+    else
+        echo "ssh install failed"
+    fi
 
+}
