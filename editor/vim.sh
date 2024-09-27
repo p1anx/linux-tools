c509e87caf7fb7174526c284f29e7b5e7570967a
xwj <sheer@XWJ-TUF.localdomain>
2024-09-27 12:33:34 +0800

    add ssh

--- editor/vim.sh
+++ editor/vim.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-
-function vim() {
+function vim_install(){
   if grep -q "ubuntu" /etc/os-release; then
     sudo apt update
     sudo apt install vim -y
   elif grep -q "rocky" /etc/os-release; then
     sudo yum update
     sudo yum install vim -y
+  elif grep -q "arch" /etc/os-release; then
+      sudo pacman -S vim
   else
     echo "The system is not supported"
     exit
   fi
+}
+
+function vim() {
+    if ! command -v vim > /dev/null 2>&1; then
+        vim_install
+    else
+        echo "[ok]vim is installed"
+    fi
 
   if [ -d "~/.vim_runtime" ]; then
     echo "amix/vimrc is configured"
