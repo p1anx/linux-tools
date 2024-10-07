071a4b03fc6e2773699c738f1f2c4be6545ac287
xwj <2514034568@qq.com>
2024-10-08 02:31:57 +0800

    update rofi for arch

--- desktop/rofi/rofi.sh
+++ desktop/rofi/rofi.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 function rofi() {
-  sudo apt update
-  sudo apt install -y rofi
+    if [[ "$XDG_SESSION_TYPE" == "x11" ]]; then
+        if grep -q "ubuntu" /etc/os-release; then
+          sudo apt update
+          sudo apt install -y rofi
+      else
+            echo -e "\e[31m[error]\e[0mrofi is not suitable for this system"
+        fi
+    elif [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
+        if grep -q "arch" /etc/os-release; then
+            sudo pacman -S rofi-wayland --noconfirm
+        else
+            echo -e "\e[31m[error]\e[0mthe system is not supported"
+        fi
+    else
+        echo -e "\e[31m[error]\e[0mDE is not supported"
+    fi
   mkdir -p ~/.config/rofi
   rofi -dump-config >~/.config/rofi/config.rasi
 
