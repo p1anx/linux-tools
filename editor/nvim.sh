a1f55c76d03b505f58584edd51ee0c418d461ac3
xwj <2514034568@qq.com>
2024-10-08 00:56:05 +0800

    update vim nvim

--- editor/nvim.sh
+++ editor/nvim.sh
@@ -3,21 +3,21 @@ function nvim_install() {
   #================================================
   # nvim and lazyvim
   #================================================
-  curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-  sudo rm -rf /opt/nvim
-  sudo tar -C /opt -xzf nvim-linux64.tar.gz
+        curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+        sudo rm -rf /opt/nvim
+        sudo tar -C /opt -xzf nvim-linux64.tar.gz
 
-  echo "export PATH=$PATH:/opt/nvim-linux64/bin" >>~/.zshrc
+        echo "export PATH=$PATH:/opt/nvim-linux64/bin" >>~/.zshrc
 
   git clone https://github.com/LazyVim/starter ~/.config/nvim
 }
 
 function nvim() {
-  if [ ! -e "/opt/nvim-linux64/bin/nvim" ]; then
+    if [ ! -e "/opt/nvim-linux64/bin/nvim" ]; then
     nvim_install
-  else
-    echo "nvim is already installed"
-  fi
+    else
+    echo "[ok]nvim is install"
+    fi
 
   if [ -e "nvim-linux64.tar.gz" ]; then
     sudo rm -rf nvim-linux64.tar.gz
@@ -27,7 +27,7 @@ function nvim() {
   fi
 
   echo "=================================="
-  echo "      neovim setup done"
+  echo "      neovim config done"
   echo "=================================="
 
 }
