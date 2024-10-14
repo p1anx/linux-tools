0af5cba1f188f149e3737c45b47d61db5bf87dd5
wsl <2514034568@qq.com>
2024-10-15 01:07:13 +0800

    update

--- desktop/aur/archlinuxcn.sh
+++ desktop/aur/archlinuxcn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo tee -a /etc/pam.conf <<EOF
 [archlinuxcn]
-Server = https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/$arch
+Server = https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/\$arch
 EOF
 sudo pacman -Sy archlinuxcn-keyring
 sudo pacman -S yay --noconfirm
