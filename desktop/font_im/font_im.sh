6c409a3a467158b58aefa006764a73c482a0ceed
wsl <2514034568@qq.com>
2024-10-15 01:33:25 +0800

    update im

--- desktop/font_im/font_im.sh
+++ desktop/font_im/font_im.sh
@@ -35,4 +35,15 @@ export GTK_IM_MODULE=fcitx
 export XMODIFIERS=@im=fcitx
 EOF
 
+  if [ ! -f ~/.local/share/fcitx5/rime/default.custom.yaml ]; then
+    mkdir -p ~/.local/share/fcitx5/rime/
+    touch ~/.local/share/fcitx5/rime/default.custom.yaml
+  fi
+  tee -a ~/.local/share/fcitx5/rime/default.custom.yaml << EOF
+  patch:
+  # 仅使用「雾凇拼音」的默认配置，配置此行即可
+  __include: rime_ice_suggestion:/
+  # 以下可根据自己所需进行自定义，仅做参考。
+  EOF
+
 }
