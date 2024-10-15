9f69548c0ba2da46d2b0655c0001cdf5bf8c124e
wsl <2514034568@qq.com>
2024-10-15 10:48:35 +0800

    update hyprland

--- desktop/hyprland/hyprland_config.sh
+++ desktop/hyprland/hyprland_config.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-HYPRLAND="$(dirname "$(realpath "\$0")")"
-source $HYPRLAND/aur/aur.sh
-source $HYPRLAND/font_im/font_im.sh
-source $HYPRLAND/waybar/waybar_config.sh
-source $HYPRLAND/hypr/hypr_config.sh
+function hyprland_config() {
+  HYPRLAND="$(dirname "$(realpath "\$0")")"
+  source $HYPRLAND/aur/aur.sh
+  source $HYPRLAND/font_im/font_im.sh
+  source $HYPRLAND/waybar/waybar_config.sh
+  source $HYPRLAND/hypr/hypr_config.sh
 
-aur
-font_im
-waybar_config
-hypr_config
+  aur
+  font_im
+  waybar_config
+  hypr_config
+
+}
