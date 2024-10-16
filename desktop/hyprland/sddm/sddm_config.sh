ca9abeb2ba00f96417d6c75bd3470791c9da777a
wsl <2514034568@qq.com>
2024-10-16 22:55:18 +0800

    udpate sddm

--- desktop/hyprland/sddm/sddm_config.sh
+++ desktop/hyprland/sddm/sddm_config.sh
@@ -4,18 +4,43 @@
 #  exit 1
 #fi
 #yay -S sddm-theme-corners-git
-git clone https://github.com/aczw/sddm-theme-corners.git ~/sddm-theme-corners
-cd ~/sddm-theme-corners/
-sudo cp -r corners/ /usr/share/sddm/themes/
 
-if [ ! -d /etc/sddm.conf.d ]; then
-  sudo mkdir -p /etc/sddm.conf.d
-  sudo touch /etc/sddm.conf.d/theme.conf
-fi
-if [ ! -f /etc/sddm.conf.d/theme.conf ]; then
-  sudo touch /etc/sddm.conf.d/theme.conf
-fi
-sudo tee /etc/sddm.conf.d/theme.conf << EOF
+function sddm_config0() {
+  git clone https://github.com/aczw/sddm-theme-corners.git ~/sddm-theme-corners
+  cd ~/sddm-theme-corners/
+  sudo cp -r corners/ /usr/share/sddm/themes/
+
+  if [ ! -d /etc/sddm.conf.d ]; then
+    sudo mkdir -p /etc/sddm.conf.d
+    sudo touch /etc/sddm.conf.d/theme.conf
+  fi
+  if [ ! -f /etc/sddm.conf.d/theme.conf ]; then
+    sudo touch /etc/sddm.conf.d/theme.conf
+  fi
+  sudo tee /etc/sddm.conf.d/theme.conf <<EOF
 [Theme]
 Current=corners
 EOF
+
+}
+
+function sddm_config() {
+  sudo pacman -S qt5-graphicaleffects --noconfirm
+  sudo pacman -S qt5-quickcontrols2 --noconfirm
+  sudo pacman -S qt5-svg --noconfirm
+
+  git clone https://github.com/JaKooLit/simple-sddm.git ~/simple-sddm
+  sudo mv ~/simple-sddm /usr/share/sddm/themes/
+
+  if [ ! -d /etc/sddm.conf.d ]; then
+    sudo mkdir -p /etc/sddm.conf.d
+    sudo touch /etc/sddm.conf.d/theme.conf
+  fi
+  if [ ! -f /etc/sddm.conf.d/theme.conf ]; then
+    sudo touch /etc/sddm.conf.d/theme.conf
+  fi
+  sudo tee /etc/sddm.conf.d/theme.conf <<EOF
+[Theme]
+Current=simple-sddm
+EOF
+}
