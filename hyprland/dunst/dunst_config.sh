e21e6d4291ad92e4d75e4edc539dd6afe2219e6d
Xu Wenjing <80949489+p1anx@users.noreply.github.com>
2024-11-06 19:11:41 +0800

    Update dunst_config.sh

--- hyprland/dunst/dunst_config.sh
+++ hyprland/dunst/dunst_config.sh
@@ -2,6 +2,16 @@
 function dunst_config() {
   local script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
   echo "waybar_config.sh脚本所在的目录是: $script_dir"
+  sudo pacman -S --noconfirm dunst
+  # example config /usr/share/dunst/dunstrc
+  
+  # Icon theme dir /usr/share/icons/
+  # sudo pacman -R --noconfirm gnome-icon-theme
+  sudo pacman -S --noconfirm papirus-icon-theme
+  # save path /usr/share/icons/Papirus/
+  
+  # notify-send
+  sudo pacman -S --noconfirm libnotify
 
   if [ ! -d ~/.config/dunst ]; then
     mkdir -p ~/.config/dunst
