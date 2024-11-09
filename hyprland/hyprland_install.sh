a0bf6568a7757d33f2f71920c81befd25a1252f8
wsl <2514034568@qq.com>
2024-11-09 12:44:48 +0800

    update install

--- hyprland/hyprland_install.sh
+++ hyprland/hyprland_install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 function hyprland_install() {
-  sudo pacman -S --noconfirm swww waybar foot dunst wlogout neofetch fzf
+  sudo pacman -S --noconfirm swww
+  sudo pacman -S --noconfirm waybar
+  sudo pacman -S --noconfirm foot
+  sudo pacman -S --noconfirm dunst
+  sudo pacman -S --noconfirm dunst
+  sudo pacman -S --noconfirm wlogout
+  sudo pacman -S --noconfirm fzf
+  sudo pacman -S --noconfirm neofetch
 }
 # hyprland_install
