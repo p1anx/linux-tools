45a89523cf516ce364b3c8fbc4427feff9f74716
wsl <2514034568@qq.com>
2024-11-09 12:05:46 +0800

    update kitty

--- hyprland/kitty/kitty_config.sh
+++ hyprland/kitty/kitty_config.sh
@@ -3,9 +3,8 @@ function kitty_config() {
   local script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
   if [ -d ~/.config/kitty ]; then
     rm -rf ~/.config/kitty
-  else
-    # mkdir ~/.config/kitty
-    cp -r $script_dir/* ~/.config
   fi
+  mkdir ~/.config/kitty
+  cp -r $script_dir/* ~/.config
 
 }
