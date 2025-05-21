9260f778204f9a4210eac297f242a36925c87f70
tuf-ubuntu <2514034568@qq.com>
2025-05-21 16:38:28 +0800

    update grub

--- grub/grub.sh
+++ grub/grub.sh
@@ -2,6 +2,8 @@
 
 function grub(){
   local script_dir=$(dirname $(realpath $0))
+  cd "$script_dir"
+  wget https://github.com/p1anx/linux-tools/releases/download/grub-theme/Elegant-wave-window-grub-themes.tar.xz
   tar -xf $script_dir/Elegant-wave-window-grub-themes.tar.xz
   cd "$script_dir/Elegant-wave-window-grub-themes/left-dark-4k"
   bash install.sh
