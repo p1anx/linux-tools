a92aba324edc8faa9ee97c380a7201e88aa418eb
tuf-ubuntu <2514034568@qq.com>
2025-06-13 10:45:25 +0800

    change alacritty and grub

--- grub/grub.sh
+++ grub/grub.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-function grub(){
+function grub() {
   local script_dir=$(dirname $(realpath $0))
   cd "$script_dir"
   wget https://github.com/p1anx/linux-tools/releases/download/grub-theme/Elegant-wave-window-grub-themes.tar.xz
   tar -xf $script_dir/Elegant-wave-window-grub-themes.tar.xz
-  cd "$script_dir/Elegant-wave-window-grub-themes/left-dark-4k"
+  # cd "$script_dir/Elegant-wave-window-grub-themes/left-dark-4k"
+  cd "$script_dir/Elegant-wave-window-grub-themes/left-dark-1080p"
   sudo bash install.sh
 }
