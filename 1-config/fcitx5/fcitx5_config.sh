59285dd6aea8cc3baa2d7d8a35cb2b4ee7f3b5ce
tuf-ubuntu <2514034568@qq.com>
2025-06-14 03:09:24 +0800

    fcitx5 config

--- 1-config/fcitx5/fcitx5_config.sh
+++ 1-config/fcitx5/fcitx5_config.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+source $G_MAIN_DIR/shell_scripts/basic.sh
+
 function fcitx5_config() {
-  local script_dir=$(dirname $(realpath $0))
+  local script_dir=$(get_dir)
   git clone https://github.com/thep0y/fcitx5-themes.git $HOME/fcitx5-themes
   # cd fcitx5-themes
   if [[ ! -f $HOME/.local/share/fcitx5/themes ]]; then
     mkdir -p $HOME/.local/share/fcitx5/themes
   fi
   cp $HOME/fcitx5-themes/* $HOME/.local/share/fcitx5/themes -r
-  cp $script_dir/../fcitx5 $HOME/.config/ -r
+  cp $script_dir $HOME/.config/ -r
   rm -rf $HOME/fcitx5-themes
 }
