42e71727b28b1634a7466a1e44e63dd161cec0c0
tuf-ubuntu <2514034568@qq.com>
2025-06-28 21:08:56 +0800

    feat: add dependency for linux

--- others/dependency.sh
+++ others/dependency.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+function ubuntu_requiremnet() {
+  sudo apt update
+  sudo apt install -y libfuse2 # for appimage
+  sudo apt install -y build-essential curl wget git vim openssh-server
+  sudo /etc/init.d/ssh start
+
+}
 function dependency() {
 
   if grep -q "ubuntu" /etc/os-release; then
-    sudo apt update
-    sudo apt install -y libfuse2 # for appimage
+    ubuntu_requiremnet
   elif grep -q "rocky" /etc/os-release; then
     sudo yum update
     sudo yum install -y libfuse2
