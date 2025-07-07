60fed2fac79310e78d0f1bac846796e0d859fcdb
tuf-ubuntu <2514034568@qq.com>
2025-07-07 15:16:16 +0800

    feat: add rofi install from source

--- rofi/rofi.sh
+++ rofi/rofi.sh
@@ -20,6 +20,44 @@ function rofi_install() {
   fi
   return 0
 }
+function rofi_install_from_source() {
+
+  local ROFI_VERSION="1.7.9.1"
+  sudo apt update
+  sudo apt install -y build-essential meson ninja-build pkg-config \
+    flex bison check libpango1.0-dev libcairo2-dev libglib2.0-dev \
+    libgdk-pixbuf2.0-dev libstartup-notification0-dev \
+    libxkbcommon-dev libxkbcommon-x11-dev \
+    libxcb-util-dev libxcb-ewmh-dev libxcb-icccm4-dev \
+    libxcb-cursor-dev libxcb-xinerama0-dev libxcb-randr0-dev \
+    libxcb-xkb-dev libx11-xcb-dev xcb-proto \
+    libxcb-image0-dev libxcb-util0-dev libxcb-xrm-dev \
+    libwayland-dev wayland-protocols
+  sudo apt install -y \
+    libxcb-keysyms1-dev \
+    libxcb-util0-dev \
+    libxcb-icccm4-dev \
+    libxcb-ewmh-dev \
+    libxcb-cursor-dev \
+    libxcb-xinerama0-dev \
+    libxcb-xkb-dev \
+    libxkbcommon-dev \
+    libxkbcommon-x11-dev \
+    libpango1.0-dev \
+    libcairo2-dev \
+    libglib2.0-dev \
+    libgdk-pixbuf-2.0-dev \
+    libstartup-notification0-dev \
+    libwayland-dev \
+    wayland-protocols
+  wget https://github.com/davatorium/rofi/releases/download/1.7.9.1/rofi-"$ROFI_VERSION".tar.gz
+  tar xf rofi-"$ROFI_VERSION".tar.gz
+  cd rofi-"$ROFI_VERSION"
+  meson setup build
+  ninja -C build
+  sudo ninja -C build install
+
+}
 function rofi_config() {
   git clone https://github.com/p1anx/rofi-themes.git
   cd rofi-themes || exit
