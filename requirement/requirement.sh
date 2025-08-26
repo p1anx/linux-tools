2b2f5cb47e0deb41411458b25ce3adfc6acae2d8
tuf-ubuntu <2514034568@qq.com>
2025-08-26 12:29:05 +0800

    feat: add arch requirement

--- requirement/requirement.sh
+++ requirement/requirement.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-function ubuntu_requiremnet() {
+function debian_requirement() {
   sudo apt update
   sudo apt install -y libfuse2 # for appimage
   sudo apt install -y build-essential curl wget git vim openssh-server flameshot fd-find
   sudo /etc/init.d/ssh start
 }
+function arch_requirement{
+  sudo pacman -Sy
+  sudo pacman -S --noconfirm curl wget git vim openssh fd base-devel
 
-function requirement() {
-
-  if grep -q "ubuntu" /etc/os-release; then
-    ubuntu_requiremnet
-  elif grep -q "rocky" /etc/os-release; then
+}
+function fedora_requirement{
     sudo yum update
     sudo yum install -y libfuse2
+}
+
+
+function requirement() {
+  if grep -q "debian" /etc/os-release; then
+    debian_requirement
+  elif grep -q "fedora" /etc/os-release; then
+    fedora_requirement
+  elif grep -q "arch" /etc/os-release; then 
+    arch_requirement
   fi
 
 }
