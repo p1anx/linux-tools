f121e1f622a597ef97a015b982649fd7b9c1f1ae
tuf-ubuntu <2514034568@qq.com>
2025-08-29 11:25:59 +0800

    feat: add qemu for arch

--- qemu/qemu.sh
+++ qemu/qemu.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-function qemu(){
+function qemu() {
 
   sudo apt update -y
   sudo apt install qemu-kvm libvirt-daemon-system virt-manager virt-viewer virtinst bridge-utils -y
   sudo apt install libguestfs-tools -y
   sudo systemctl enable libvirtd.service
   sudo systemctl start libvirtd.service
-  newgrp libvirt 
+  newgrp libvirt
   sudo usermod -aG libvirt $USER
   sudo systemctl restart libvirtd.service
   # 临时启用 kvm_intel 嵌套虚拟化
@@ -14,7 +14,21 @@ function qemu(){
   sudo modprobe kvm_intel nested=1
   # 修改配置，永久启用嵌套虚拟化
   echo "options kvm-intel nested=1" | sudo tee /etc/modprobe.d/kvm-intel.conf
-  cat /sys/module/kvm_intel/parameters/nested 
+  cat /sys/module/kvm_intel/parameters/nested
   echo "qemu is installed successfully"
 
+}
+
+function qemu_arch() {
+  sudo pacman -S qemu-full virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat
+  sudo systemctl enable libvirtd.service
+  sudo systemctl start libvirtd.service
+  sudo usermod -a -G libvirt $(whoami)
+  # 或者执行以下命令立即生效
+  newgrp libvirt
+  # 检查 CPU 虚拟化支持
+  LC_ALL=C lscpu | grep Virtualization
+
+  # 检查 KVM 模块是否加载
+  lsmod | grep kvm
 }
